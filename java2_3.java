67af9c888b2e0c17c8c3cf8e19bac61715c52021
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:11:37 +0800

    Update java2_3.java

--- java2_3.java
+++ java2_3.java
@@ -1,5 +1,15 @@
 package git_demo;
-
+import java.util.Scanner;
 public class java2_3 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter three number: ");
+    double number1 = input.nextDouble();
+    double number2 = input.nextDouble();
+    double number3 = input.nextDouble();
+    
+    double average=(number1+number2+number3)/3;
+    System.out.println("The average of " +number1+" "+number2+" "+number3+" is "+average);
+	}
 }
