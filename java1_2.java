1d42de9de010af2983cd07cf061b3ab5339bcd6f
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:07:31 +0800

    Update java1_2.java

--- java1_2.java
+++ java1_2.java
@@ -1,5 +1,10 @@
 package git_demo;
 
 public class java1_2 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Programming is fun!");
+       System.out.println("Programming is First!");
+       System.out.println("Problem Driven");
+	}
 }
