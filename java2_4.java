2cebe9c8fcf420f797e11178e86b8d3991ef3f0e
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:11:58 +0800

    Update java2_4.java

--- java2_4.java
+++ java2_4.java
@@ -1,5 +1,13 @@
 package git_demo;
-
+import java.util.Scanner;
 public class java2_4 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	final double PI =3.14159;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number for radius: ");
+    double radius = input.nextDouble();
+    double area = radius*radius*PI;
+    System.out.println("The area for the circle "+radius+" is "+area);
+	}
 }
