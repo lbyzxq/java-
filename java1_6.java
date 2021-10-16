12ef962ef33cfd75e7e149c6e766134c4e46eac3
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:10:04 +0800

    Update java1_6.java

--- java1_6.java
+++ java1_6.java
@@ -1,5 +1,9 @@
 package git_demo;
 
 public class java1_6 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Celsius 35 is Fahrenheit dregree");
+    System.out.println((9/5)*35+32);
+	}
 }
