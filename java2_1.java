ab20aef66a47aa546e7561558b40e0625cb8c05f
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:10:22 +0800

    Update java2_1.java

--- java2_1.java
+++ java2_1.java
@@ -1,5 +1,12 @@
 package git_demo;
 
 public class java2_1 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    double radius;
+    double area;
+    radius=20;
+    area=radius*radius*3.14159;
+    System.out.println("The area for the circle of radius"+radius+" is "+area);
+	}
 }
