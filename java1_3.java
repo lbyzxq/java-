2c25ddbc554a1a253be83905b40d0f51c9bab8c8
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:08:21 +0800

    Update java1_3.java

--- java1_3.java
+++ java1_3.java
@@ -1,5 +1,9 @@
 package git_demo;
 
 public class java1_3 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.print("(10.5+2*3)/(45-3.5) = ");
+    System.out.println((10.5+2*3)/(45-3.5));
+	}
 }
