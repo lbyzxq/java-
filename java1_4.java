c70cbe2cb487e9105c353a88703e56ff4853a4ee
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:09:13 +0800

    Update java1_4.java

--- java1_4.java
+++ java1_4.java
@@ -1,5 +1,9 @@
 package git_demo;
 
 public class java1_4 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Welcome to Java);
+    System.out.println("Welcome to Java);	
+   }
 }
