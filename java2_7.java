56fa60fc014fc50a2c145a17ab419b6836d72334
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:13:27 +0800

    Update java2_7.java

--- java2_7.java
+++ java2_7.java
@@ -1,5 +1,22 @@
 package git_demo;
 
 public class java2_7 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    long totalMilliseconds = System.currentTimeMillis();
+    
+    long totalSeconds = totalMilliseconds / 1000;
+    
+    long currentSecond = totalSeconds % 60;
+    
+    long totalMinutes = totalSeconds / 60;
+    
+    long currentMinute = totalMinutes % 60;
+    
+    long totalHours = totalMinutes / 60;
+    
+    long currentHours = totalHours % 24;
+    
+    System.out.println("Current time is "+currentHours + ":"+ currentMinute + ":"+ currentSecond +" GMT");
+	}
 }
