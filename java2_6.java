19d8622b33514760e94825fde6ad757cf4086e29
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:13:04 +0800

    Update java2_6.java

--- java2_6.java
+++ java2_6.java
@@ -1,5 +1,12 @@
 package git_demo;
-
+import java.util.Scanner;
 public class java2_6 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner input =new Scanner (System.in);
+    System.out.print("Enter a dgree in Fahrenheit:");
+    double fahrenheit = input.nextDouble();
+    double celsius = (5.0/9)*(fahrenheit-32);
+    System.out.println("Fahrenheit "+fahrenheit +" is "+celsius +" in Celsius");
+	}
 }
