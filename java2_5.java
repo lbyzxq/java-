40908b914daba93c7a0c8dd9134eae0e0dfdd6ed
lbyzxq <91312637+lbyzxq@users.noreply.github.com>
2021-10-16 22:12:36 +0800

    Update java2_5.java

--- java2_5.java
+++ java2_5.java
@@ -1,5 +1,13 @@
 package git_demo;
-
+import java.util.Scanner;
 public class java2_5 {
-
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter an integer for second: ");
+    int Second = input.nextInt();
+    int minutes = Second/60;
+    int remainingSecond = Second % 60;
+    System.out.println(Second+" Second is "+ minutes + " minutes and " + remainingSecond +" Second");
+	}
 }
